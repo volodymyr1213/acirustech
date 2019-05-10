d07a5204fcbb9531397d2acf757449bc657a2975
SharifAbdulCoder <abdugofir@gmail.com>
2019-05-09 19:47:17 -0500

    Abdul's branch

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,29 @@
+def acirrustech
+node("master") {
+  stage("Pull the code"){
+    git 'git@github.com:fuchicorp/acirustech.git'
+  }
+
+  stage("Build"){
+    acirrustech = docker.build("sharifabdulcoder/fuchicorp")
+
+  }
+
+
+  stage("Unites"){
+    acirrustech.inside("""
+    #!/bin/bash
+    ls /app/requirements.txt
+    cat /app/app.py
+    ls /app/templates/
+
+
+    """)
+
+  }
+
+  //
+  // stage("Deploy"){
+  // 
+  // }
+}
