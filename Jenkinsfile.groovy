3a16299c56a8570c09cc8315c1e458b1c76f71e9
SharifAbdulCoder <abdugofir@gmail.com>
2019-05-11 17:27:14 -0500

    :) All good :)

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,23 +13,3 @@ node("master") {
 
     }
 }
-
-//
-//
-//
-//
-// def acirrustech
-// node("master") {
-//   stage("Pull the code"){
-//     git 'git@github.com:fuchicorp/acirustech.git'
-//   }
-//
-//   stage("Build"){
-//     acirrustech = docker.build("sharifabdulcoder/fuchicorp")
-//
-//   }
-//   stage("Deploy"){
-//     docker.run("sharifabdulcoder/fuchicorp")
-//
-//   }
-// }
