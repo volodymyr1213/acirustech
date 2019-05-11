7d389ad8ab42cd3da6a6f4175606afc7b40ab958
SharifAbdulCoder <abdugofir@gmail.com>
2019-05-11 17:34:31 -0500

    won't work

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,9 +7,21 @@ node("master") {
   stage('Build') {
     acirustech = docker.build("sharifabdulcoder/acirustech")
   }
+  withCredentials([usernameColonPassword(credentialsId: 'ID', variable: 'docker_cred')]) {
+      // some block
+  }
+  stage('Push image') {
+
+   // Push docker image to the Docker hub
+    docker.withRegistry('docker_cred', 'dockerhub-credentials') {
+        app.push("0.1")
+        app.push("latest")
+    }
+
 
   stage("Deploy"){
       sh "docker run -dti -p 81:5000 sharifabdulcoder/acirustech"
 
     }
+
 }
