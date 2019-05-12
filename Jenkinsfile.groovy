a2d77ebbe2b9ec191c18cb98c6ba40b78ee26802
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-12 12:58:49 -0500

    change port

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,7 +20,7 @@ node("master") {
   }
 
   stage("Deploy"){
-      sh "docker run -dti -p 80:8080 50364747/centos_jenkins:latest"
+      sh "docker run -dti -p 89:8080 50364747/centos_jenkins:latest"
 
     }
 }
