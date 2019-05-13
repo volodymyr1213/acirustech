2437c7d4da945f5a03102da5d0e88de653406f17
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-05-13 18:31:58 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "Docker" {
   instance_type = "t2.micro"
   count         = "1"
   key_name      = "dilfuza"
+  region         = "eu-west-1"
 }
 
 resource "aws_route53_zone" "docker" {
